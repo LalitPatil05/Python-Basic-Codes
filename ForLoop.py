777785b229e81af60263757bbd43a587d60c30aa
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-26 23:08:40 +0530

    [ADDED] : Lalit - Demonstration of For Loop in Python.

---
+++ ForLoop.py
@@ -0,0 +1,15 @@
+
+# Demonstration of For Loop  in Python
+
+i = 0
+
+for i in range(0,10):
+    print(i)
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+
+sum = 0
+for val in numbers:
+    sum = sum+val
+
+print("The Sum is :", sum)
