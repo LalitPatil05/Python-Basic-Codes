26ad065d23a012ecb8eaf9a78f6a6878f0057a33
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-27 22:34:47 +0530

    [ADDED] : Lalit - Demonstration of List Comparision function inpython

---
+++ ListComparison.py
@@ -0,0 +1,15 @@
+# Demonstratin of comparison of List in python
+
+list1 = [1,2,3]
+list2 = [4,5,6,7]
+
+#result = cmp(list1,list2)
+
+#print(len(list1))
+#print(len(list2))
+
+print(min(list1))
+print(max(list1))
+
+print(min(list2))
+print(max(list2))
