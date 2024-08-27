cf054cbb3660204a8713f9135d96b0d1192c23d3
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-27 22:43:03 +0530

    [ADDED] : Lalit - Demonstration of Copy a List in python

---
+++ ListCopyFunction.py
@@ -0,0 +1,10 @@
+# Demonstration of List Copy inbuilt function in python
+# using copy() function we can copy a one list item into another list..
+
+list1 = [1,2,3,5,6,7]
+
+print("List 1 is : ",list1)
+
+list2 = list1.copy()
+
+print("List 2 is : ",list2)
