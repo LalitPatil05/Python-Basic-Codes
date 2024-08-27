0f69552885256548c78f90fd68844b11d0ce618a
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-27 22:45:07 +0530

    [ADDED] : Lalit - Demonstration of Membership in operator in python

---
+++ Membershipin.py
@@ -0,0 +1,9 @@
+# Demonstration of memebership in function in python...
+# this operator return a true if character is present
+# otherwise its return a false...
+
+string = "Python"
+
+print('n' in string)
+
+print( 'x' in string)
