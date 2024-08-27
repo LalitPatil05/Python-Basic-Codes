b7f8656e7d7440e34ef8e72708b6ed310bb8d572
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-27 22:38:48 +0530

    [ADDED] : Lalit - Demonstration of Copy a List in python

---
+++ ListCopy.py
@@ -0,0 +1,11 @@
+WeekDays = ['Sunday','Monday','Tuesday','Wendsday','Thursday','Friyday','Saturday']
+
+"""print(WeekDays[0])
+
+print(len(WeekDays))
+
+print(type(WeekDays))
+
+WeekDays[0] = 'Funday'
+
+print(WeekDays)"""
