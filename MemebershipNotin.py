3585833fe2df71fa74687025ba3431db97ea2034
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-28 20:31:26 +0530

    [ADDED] : Lalit - Demonstration of Membership Not in Operator in Python.

---
+++ MemebershipNotin.py
@@ -0,0 +1,9 @@
+# Demonstration of Membership not in concept in python
+# it returns the True value if character is not present
+# otherwise if character is present it returns false value..
+
+string = "python"
+
+print(('n' not in string))
+
+print('x' not in string)
