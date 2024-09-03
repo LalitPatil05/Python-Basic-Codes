eb95e2bc91d1fb2cf1439093f390d7d289dfb892
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-03 23:11:57 +0530

    [ADDED] : Demonstration of clear() method in dictionary in python.

---
+++ cleardict.py
@@ -0,0 +1,15 @@
+# demonstration of clear() method in dictionary to clear the dictionary
+# using the specified key can also delete a particular item on dictionary.
+# and also use clear() you can also clear the dictionary for all items.\
+
+dict = {
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+
+print(dict)
+
+dict.clear()
+
+print(dict)
