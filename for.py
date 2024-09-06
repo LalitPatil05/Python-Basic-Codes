efbf52fde2e564c31bfe6c18337d712b71db6e54
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-06 11:37:55 +0530

    [ADDED] : Lalit - Demonstration of for loop.

---
+++ for.py
@@ -0,0 +1,17 @@
+for x in range(10):
+    print(x)
+
+print("********")
+
+s=sum(range(6))
+print(s)
+
+print("Even Numbers :")
+for x in range(20):
+    if(x%2==0):
+        print(x)
+
+print("Odd Numbers :")
+for x in range(20):
+    if(x%2==1):
+        print(x)
