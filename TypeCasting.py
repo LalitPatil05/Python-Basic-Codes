3ab7228a4f2ac0dea74b68dbe48fbb5e14f9e066
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-17 17:34:34 +0530

    [ADDED] : Lalit - Demonstration of Type Casting in Python.

---
+++ TypeCasting.py
@@ -0,0 +1,11 @@
+# Demonstration of Type Casting in Python
+
+a = input("Enter the Value of a : ")
+
+# Before Type Casting the Type of a is String
+print(type(a))
+
+# After Type Casting the value of a is int
+
+a1 = int(a)
+print(type(a1))
