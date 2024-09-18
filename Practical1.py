75807a3389eaa4f0c9059e808d663d6854b7323c
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-19 00:53:26 +0530

    [ADDED] : Lalit - Demonstration of Arithmetic Operators in Java

---
+++ Practical1.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+a = 20
+b = 10
+
+c = a + b
+print("Addition is : ",c)
+
+c = a - b
+print("Substraction is : ",c)
+
+c = a * b
+print("Multiplication is : ",c)
+
+c = a / b
+print("Divison is : ",c)
+
+c = a % b
+print("Remainder is : ",c)
+
