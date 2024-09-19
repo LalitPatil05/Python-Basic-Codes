70828951c34f70b67d51db1b991c19c64ab016fc
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-20 00:29:35 +0530

    [ADDED] : Lalit - Demonstration of Function as a Argument in Python.

---
+++ Function_as_Argument.py
@@ -0,0 +1,14 @@
+def Greet():
+    print("Hello Python")
+
+def Greet2():
+    print("Programming")
+
+def apply(fun):
+    return fun
+
+def apply2(fun1):
+    return fun1
+
+apply(Greet())
+apply2(Greet2())
