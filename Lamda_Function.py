d2e069dbed534b4a9b2a076b9140e4f2fc67b7ca
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-20 00:19:52 +0530

    [ADDED] : Lalit - Demonstration of Lamda Function in Python.

---
+++ Lamda_Function.py
@@ -0,0 +1,13 @@
+# Example of using lambda functions in Python 
+
+# Lambda function that adds 1 to x 
+add = lambda x: x + 1 
+
+ # Lambda function that multiplies x by 2 
+multiply = lambda x: x * 2 
+
+result1 = add(3) # Result: 4 
+result2 = multiply(3) # Result: 6 
+
+print(result1)
+print(result2)
