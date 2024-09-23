c79425a64bb8cc026116cd827e1b2b6d3734cc7c
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-23 20:56:32 +0530

    [ADDED] : Lalit - Demonstration of Map Function in Python

---
+++ MapFunction.py
@@ -0,0 +1,7 @@
+l = [1,2,3,4] # Declare the List
+
+square = list(map(lambda x : x * x, l)) # Use of map Function
+cube = list(map(lambda x : x * x * x, l)) # Use of map function
+
+print("Square List is : ",square)
+print("Cube List is : ",cube)
