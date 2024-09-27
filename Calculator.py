d63cb311f31fb84e55d38303118c61739adf64d4
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-27 19:53:33 +0530

    [ADDED] : Lalit - import Package in Java.

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+def calculation(a,b):
+    c = a + b
+    print("Addition is : ",c)
+
+    c = a - b
+    print("Substraction is : ",c)
+
+    c = a * b
+    print("Multiplication is : ",c)
+
+    c = a / b
+    print("Divison is : ",c)
