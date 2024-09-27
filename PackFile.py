f5b9f9948768d12e998b9e952c8cb08b5902a45a
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-27 19:21:39 +0530

    [ADDED] : Lalit - Demonstration of Package in Python.

---
+++ PackFile.py
@@ -0,0 +1,9 @@
+from PythonPack import ArithmeticPack, Square , Cube
+
+print("Addition is : ",ArithmeticPack.add(2,4))
+print("Subsraction is : ",ArithmeticPack.sub(4,2))
+print("Multiplication is : ",ArithmeticPack.mul(2,4))
+print("Divison is : ",ArithmeticPack.div(4,2))
+
+print("Square is : ",Square.square(2))
+print("Cube is : ",Cube.cube(2))
