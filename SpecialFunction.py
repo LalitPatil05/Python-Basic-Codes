9b5060d06ab08b0d50f3884af5f0eca3c6d36fd5
LalitPatil05 <lalitpatil4636@gmail.com>
2026-03-29 22:04:07 +0530

    [ADDED] : Lalit - Demonstration of pre defined function in python to manipulate the sequence data types.

---
+++ SpecialFunction.py
@@ -0,0 +1,14 @@
+# Linear Search
+arr = [10, 20, 30, 40]
+key = 30
+for i in arr:
+    if i == key:
+        print("Found")
+
+# Sorting
+arr.sort()
+print("Sorted:", arr)
+
+# Hash Table (Dictionary)
+data = {"id": 101, "name": "Lalit"}
+print(data["name"])
