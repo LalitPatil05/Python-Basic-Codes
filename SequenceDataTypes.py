0768293eaa4ade6ba0ebff459ae17ed6726db166
LalitPatil05 <lalitpatil4636@gmail.com>
2026-03-29 21:55:43 +0530

    [ADDED] : Lalit - Demonstration of Sequence Data Types in Python.

---
+++ SequenceDataTypes.py
@@ -0,0 +1,12 @@
+# List
+lst = [1, 2, 3, 4]
+lst.append(5)
+print("List:", lst)
+
+# Tuple
+tup = (10, 20, 30)
+print("Tuple:", tup)
+
+# Dictionary
+d = {"name": "Lalit", "age": 22}
+print("Dictionary:", d)
