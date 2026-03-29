431cba8636740bf95f2c0af0fc8f805b737b9476
LalitPatil05 <lalitpatil4636@gmail.com>
2026-03-29 21:54:24 +0530

    [ADDED] : Lalit - Demonstration of Control Statements in Python.

---
+++ ControlStatementsDemo.py
@@ -0,0 +1,10 @@
+# Check even or odd using if-else
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Loop example
+for i in range(1, 6):
+    print(i)
